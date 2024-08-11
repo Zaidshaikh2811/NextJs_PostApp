b308d55b4c7396310604e0520a21d40312c1afb0
Zaidshaikh2811 <zaidshaikhhulk@gmail.com>
2024-08-11 11:14:18 +0530

    finalDeploy

---
+++ app/new-post/page.js
@@ -0,0 +1,11 @@
+"use client"
+
+import PostForm from '@/components/post-form';
+import { createPost } from '@/actions/posts';
+
+
+export default function NewPostPage() {
+
+  return <PostForm createPost={createPost}></PostForm>
+
+}
